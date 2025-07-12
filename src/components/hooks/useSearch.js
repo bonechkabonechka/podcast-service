007dc749baffbf420739ec836e80bbdbd3503717
Goebbels <bonechkaskripit@gmail.com>
2025-07-12 22:51:28 +0700

    debounced search

--- src/components/hooks/useSearch.js
+++ src/components/hooks/useSearch.js
@@ -1,11 +1,15 @@
 import { useMemo } from 'react'
+import { useDebounce } from './useDebounce'
 
 export const useSearch = (items, searchValue) => {
+    const debouncedSearch = useDebounce(searchValue)
     const filteredItems = useMemo(() => {
-        if (!searchValue.trim()) return items
-        
-        return items.filter(obj => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
-    }, [items, searchValue])
-    
+        if (!debouncedSearch.trim()) return items
+
+            const query = debouncedSearch.toLowerCase()
+
+            return items.filter(obj => obj.name.toLowerCase().includes(query) ||
+                                obj.description.toLowerCase().includes(query))
+    }, [items, debouncedSearch])
     return filteredItems
 }
